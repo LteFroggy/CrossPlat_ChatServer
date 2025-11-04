7404a02809b5a4b02a2d36c20587872c16b322aa
hojong <33508056+LteFroggy@users.noreply.github.com>
2025-11-04 23:55:41 +0900

    feat(#3) : Windows용 서버소켓 추가(미완)

--- server/include/socket/WinServerSocket.h
+++ server/include/socket/WinServerSocket.h
@@ -1,8 +1,19 @@
-//
-// Created by zac03 on 2025-10-23.
-//
+#pragma once
+#include <thread>
 
-#ifndef HELLO_WINSERVERSOCKET_H
-#define HELLO_WINSERVERSOCKET_H
+#include "socket/IServerSocket.h"
 
-#endif //HELLO_WINSERVERSOCKET_H
+class WinServerSocket : public IServerSocket {
+private:
+    static WinServerSocket* instance; // 싱글톤을 위한 instance
+    std::atomic<bool> running_{false}; // 서버 실행중인가?
+    int listen_fd_ = -1; // 열린 서버 핸들
+    std::thread th_; // 실행할 스레드
+
+    // 외부 초기화 방지
+    WinServerSocket() = default;
+public :
+    static WinServerSocket* getInstance();
+    int start() override;
+    void stop() override;
+}
