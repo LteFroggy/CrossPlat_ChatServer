e2a6d4334e732a4cb8d8b5b8f2f6f27794b7c8a7
LteFroggy <zac0328@naver.com>
2025-11-06 16:27:19 +0900

    refactor(#8) : Client 테스트 코드 추가

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+#include "socket/MacClientSocketMaker.h"
+
 using namespace std;
 
 int main() {
-    cout << "Hello, World! I'm Client" << endl;
+    MacClientSocketMaker socketMaker = MacClientSocketMaker();
+
+    int socket_fd = socketMaker.createSocket();
+    socketMaker.deleteSocket();
 }
